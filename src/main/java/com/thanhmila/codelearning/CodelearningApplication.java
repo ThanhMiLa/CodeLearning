4c0f9292a0257a328d160dc2681a0ec360db3611
Thanh_MiLa <vntvlogs@gmail.com>
2026-04-28 22:16:34 +0700

    Initial commit

---
+++ src/main/java/com/thanhmila/codelearning/CodelearningApplication.java
@@ -0,0 +1,13 @@
+package com.thanhmila.codelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanhmila/codelearning/CodelearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhmila.codelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
