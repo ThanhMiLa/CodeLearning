5b2e836f974dd49749aab3f990a5609554284ac5
ThanhMiLa <vntvlogs@gmail.com>
2026-07-27 19:03:09 +0700

    fix: change fallback URL to judge0-codelearning-server

--- backend/scratch.java
+++ backend/scratch.java
@@ -1,8 +1,11 @@
 import java.net.URI;
 public class scratch {
     public static void main(String[] args) {
-        URI uri = URI.create("http://judge0_codelearning_server:2358/submissions/batch?base64_encoded=false");
-        System.out.println("Host: " + uri.getHost());
-        System.out.println("Authority: " + uri.getAuthority());
+        try {
+            URI uri = URI.create(" http://judge0-server:2358/submissions/batch?base64_encoded=false");
+            System.out.println("Host: " + uri.getHost());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
